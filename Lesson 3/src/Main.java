72d28013cb80c4a85f8ece5e98851a76be748fef
dimonzhinbox <dimon.zh1984@gmail.com>
2024-09-29 19:46:22 +0300

    file creation Lesson 3

---
+++ Lesson 3/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Добро пожаловать в конвертер ГБ");
+//        System.out.println("В " + 4 +"Гб: " + (4*1024) + "Мб");
+
+        // масса: кг. / рост2 (м2)
+        System.out.println("\t\t\t***Добро пожаловать в калькулятор BMI***");
+        System.out.println("BMI:" + 75.0/(180*180));
+    }
+}
